ed7185723173c61c687a0150f14a8954f54a766d
Pavel Vojacek <pavelvojacek@gmail.com>
2015-05-26 17:00:40 +0200

    trying....

--- src/android/cordovaKiosk.java
+++ src/android/cordovaKiosk.java
@@ -6,25 +6,23 @@ import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import android.os.Process;
-import java.lang.Runtime;
 
 /**
  * This class echoes a string called from JavaScript.
  */
-public class cordovaKiosk extends CordovaPlugin{
+public class cordovaKiosk extends CordovaPlugin {
 
 @Override
 public boolean execute(String action,JSONArray args,CallbackContext callbackContext)throws JSONException{
         if(action.equals("hideBars")){
-        String message=args.getString(0);
-        this.hideMethod(message,callbackContext);
-        return true;
+                String message=args.getString(0);
+                this.showBars(message,callbackContext);
+                return true;
         }
         if(action.equals("showBars")){
-        String message=args.getString(0);
-        this.showMethod(message,callbackContext);
-        return true;
+                String message=args.getString(0);
+                this.showBars(message,callbackContext);
+                return true;
         }
         return false;
         }
