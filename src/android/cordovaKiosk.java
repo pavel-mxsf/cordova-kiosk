45ce0c5251002f9dbdef4e32ad6923a1777b81c2
Pavel Vojacek <pavelvojacek@gmail.com>
2015-05-26 18:10:37 +0200

    code cleanup

--- src/android/cordovaKiosk.java
+++ src/android/cordovaKiosk.java
@@ -9,59 +9,53 @@ import org.json.JSONObject;
 import android.util.Log;
 
 /**
- * This class echoes a string called from JavaScript.
+ * show and hide android toolbars.
  */
 public class cordovaKiosk extends CordovaPlugin {
 
-@Override
-public boolean execute(String action,JSONArray args,CallbackContext callbackContext)throws JSONException{
-        if(action.equals("hideBars")){
+	@Override
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+		if (action.equals("hideBars")) {
 
-                this.hideBars(callbackContext);
-                return true;
-        }
-        if(action.equals("showBars")){
+			this.hideBars(callbackContext);
+			return true;
+		}
+		if (action.equals("showBars")) {
 
-                this.showBars(callbackContext);
-                return true;
-        }
-        return false;
-        }
+			this.showBars(callbackContext);
+			return true;
+		}
+		return false;
+	}
 
-private void hideBars(CallbackContext callbackContext){
+	private void hideBars(CallbackContext callbackContext) {
 
-        try
-        {
-        String command;
-        String[] envp = null;
-               command = "service call activity 42 s16 com.android.systemui";
-               Process proc = Runtime.getRuntime().exec(new String[] { "su", "-c", command }, envp);
-               proc.waitFor();
-        callbackContext.success("ok");
-        }
-        catch(Exception ex)
-        {
+		try {
+			String command;
+			String[] envp = null;
+			command = "service call activity 42 s16 com.android.systemui";
+			Process proc = Runtime.getRuntime().exec(new String[] {
+				"su", "-c", command
+			}, envp);
+			proc.waitFor();
+			callbackContext.success("ok");
+		} catch (Exception ex) {
+			callbackContext.error("root needed");
+		}
+	}
 
-        //Log.e("ROOTERROR",ex.getMessage());
-        callbackContext.error("root needed");
-        }
-        }
-
-private void showBars(CallbackContext callbackContext){
-        try
-                {
-               String command;
-               String[] envp = null;
-                       command = "am startservice -n com.android.systemui/.SystemUIService";
-                       Process proc = Runtime.getRuntime().exec(new String[] { "su", "-c", command }, envp);
-                       proc.waitFor();
-                      callbackContext.success("ok");
-                }
-                catch(Exception ex)
-                {
-
-                //Log.e("ROOTERROR",ex.getMessage());
-                callbackContext.error("root needed");
-                }
-        }
+	private void showBars(CallbackContext callbackContext) {
+		try {
+			String command;
+			String[] envp = null;
+			command = "am startservice -n com.android.systemui/.SystemUIService";
+			Process proc = Runtime.getRuntime().exec(new String[] {
+				"su", "-c", command
+			}, envp);
+			proc.waitFor();
+			callbackContext.success("ok");
+		} catch (Exception ex) {
+			callbackContext.error("root needed");
+		}
+	}
 }
